fd0b1109a483bb8c9911c8ded95542e3084241af
Flora Pierre <fpierre1202@eagle.fgcu.edu>
2020-06-09 12:15:38 -0400

    Updates

--- Assets/Value.cs
+++ Assets/Value.cs
@@ -9,7 +9,8 @@ public class Value : MonoBehaviour
 
     void Start()
     {
-        Instantiate(CubeNumberPrefab, transform.position, Quaternion.identity, transform);
+        Vector3 offset = new Vector3(0, 0, 1);
+        Instantiate(CubeNumberPrefab, transform.position + offset , Quaternion.identity, transform);
     }
     
 }
